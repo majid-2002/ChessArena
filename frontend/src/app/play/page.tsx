9deb11cc8f48eafcbe4c348b434c54b03b117fb3
Abdul Majid <majid616365@gmail.com>
2023-08-26 00:04:12 +0530

    update

--- frontend/src/app/play/page.tsx
+++ frontend/src/app/play/page.tsx
@@ -12,73 +12,34 @@ export default function Playpage() {
   const chess = new Chess();
   const [boardArray, setBoardArray] = useState(chess.board());
 
-  console.log(boardArray);
+  let board = [];
+  const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
+  const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
+
+  for (let i = verticalAxis.length - 1; i >= 0; i--) {
+    for (let j = 0; j < horizontalAxis.length; j++) {
+      board.push(horizontalAxis[j] + verticalAxis[i]);
+    }
+  }
+
+  console.log(board);
 
   return (
     <div className="w-full h-screen flex flex-col sm:flex-row items-center justify-center sm:space-x-5 space-y-8 p-5">
       {/* Chessboard */}
       <div className="relative sm:w-1/2 md:2/3 w-full justify-center flex items-center">
         <Image src={chessboard} alt="Chessboard" className="w-full" />
-        <div className="grid grid-cols-8 grid-rows-8 absolute top-0 w-full">
-          {boardArray.map((row: any, rowIndex: number) => {
-            return row.map((piece: any, colIndex: number) => {
-              return (
-                <div
-                  key={colIndex}
-                  className="w-full flex items-center justify-center h-full"
-                >
-                  {piece?.color == "b" && (
-                    <Image
-                      src={pieceImageData(piece.type, piece.color)}
-                      alt="piece"
-                      width={100}
-                      height={100}
-                      className="w-full h-full"
-                    />
-                  )}
-                </div>
-              );
-            });
-          })}
-          {boardArray.map((row: any, rowIndex: number) => {
-            return row.map((piece: any, colIndex: number) => {
-              return (
-                <div
-                  key={colIndex}
-                  className="w-full flex items-center justify-center h-full"
-                >
-                  {piece == null && (
-                    <Image
-                      src={pieceImageData(piece?.type, piece?.color)}
-                      alt="piece"
-                      width={100}
-                      height={100}
-                      className="w-full h-full vis"
-                    ></Image>
-                  )}
-                </div>
-              );
-            });
-          })}
-          {boardArray.map((row: any, rowIndex: number) => {
-            return row.map((piece: any, colIndex: number) => {
-              return (
-                <div
-                  key={colIndex}
-                  className="w-full flex items-center justify-center h-full"
-                >
-                  {piece?.color == "w" && (
-                    <Image
-                      src={pieceImageData(piece.type, piece.color)}
-                      alt="piece"
-                      width={100}
-                      height={100}
-                      className="w-full h-full"
-                    />
-                  )}
-                </div>
-              );
-            });
+        <div className="grid grid-cols-8 grid-rows-8 absolute top-0 w-full h-full">
+          {board.map((piece, index) => {
+            
+            return (
+              <div
+                key={index}
+                className="w-full h-full flex items-center justify-center"
+              >
+                {piece}
+              </div>
+            );
           })}
         </div>
       </div>
