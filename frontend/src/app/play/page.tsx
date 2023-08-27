0a2ff10d0b9a3780542c41d54efcee721b2567cd
Abdul Majid <majid616365@gmail.com>
2023-08-27 12:49:42 +0530

    f: updated the board to boardArray

--- frontend/src/app/play/page.tsx
+++ frontend/src/app/play/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import React, { useEffect, useState } from "react";
+import React, { use, useEffect, useState } from "react";
 import chessboard from "@/../public/images/board.png";
 import { pieceImageData } from "@/utils/pieces";
 import convertBoardArrayToFEN from "@/utils/FENConfig";
@@ -14,19 +14,20 @@ export default function Playpage() {
   const [currentPosition, setCurrentPosition] = useState<string>("");
   const [moves, setMoves] = useState<string[]>([]);
 
-  let board = [];
-  const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
-  const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
+  // let board = [];
+  // const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
+  // const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
-  for (let i = verticalAxis.length - 1; i >= 0; i--) {
-    for (let j = 0; j < horizontalAxis.length; j++) {
-      board.push(horizontalAxis[j] + verticalAxis[i]);
-    }
-  }
+  // for (let i = verticalAxis.length - 1; i >= 0; i--) {
+  //   for (let j = 0; j < horizontalAxis.length; j++) {
+  //     board.push(horizontalAxis[j] + verticalAxis[i]);
+  //   }
+  // }
+
 
   useEffect(() => {
-    console.log(moves);
-  }, [moves]);
+    console.log(boardArray)
+  }, [boardArray]);
 
   return (
     <div className="w-full h-screen flex flex-col sm:flex-row items-center justify-center sm:space-x-5 space-y-8 p-5">
@@ -34,88 +35,95 @@ export default function Playpage() {
       <div className="relative w-full sm:w-1/2 justify-center flex items-center">
         <Image src={chessboard} alt="Chessboard" className="w-full h-full" />
         <div className="grid grid-cols-8 grid-rows-8 absolute top-0 w-full ">
-          {board.map((square, index) => {
-            const row = Math.floor(index / 8);
-            const col = square.charCodeAt(0) - "a".charCodeAt(0);
+          {boardArray.map((row: any, rowIndex: number) => {
+            return row.map((piece: any, colIndex: number) => {
+              const row = rowIndex + 1;
+              const col = colIndex + 1;
+              // start from a8 to h1
+              const square = `${String.fromCharCode(97 + colIndex)}${8 - row + 1}`;
 
-            const piece = boardArray[row][col];
-            const pieceKey = row * 8 + col; // Generate a unique key for each piece
-
-            return (
-              <div key={pieceKey} className="h-full w-full relative">
-                {piece ? (
-                  <div
-                    onClick={() => {
-                      if (moves.length === 0) {
-                        //? If no moves are available, show the moves for the piece
-                        setMoves(chess.moves({ square: piece.square })); //? Get the moves for the piece
-                        setCurrentPosition(piece.square);
-                      } else {
-                        setMoves([]);
-                        setCurrentPosition("");
+              
+              return (
+                <div key={colIndex} className="h-full w-full relative">
+                  {piece ? (
+                    <div
+                      onClick={() => {
+                        if (moves.length === 0) {
+                          setMoves(chess.moves({ square: piece.square }));
+                          setCurrentPosition(piece.square);
+                        } else {
+                          setMoves([]);
+                          setCurrentPosition("");
+                        }
+                      }}
+                    >
+                      {rowIndex === 7 && (
+                        <div
+                          className={`absolute bottom-0 right-1 text-sm ${
+                            (colIndex + 1) % 2 === 0
+                              ? "text-green-700"
+                              : "text-white"
+                          }`}
+                        >
+                          {square[0]}
+                        </div>
+                      )}
+                      {colIndex === 0 && (
+                        <div
+                          className={`absolute top-0 left-1 text-sm ${
+                            (rowIndex + 1) % 2 === 0
+                              ? "text-white"
+                              : "text-green-700"
+                          }`}
+                        >
+                          {square[1]}
+                        </div>
+                      )}
+                      <Image
+                        src={pieceImageData(piece.type, piece.color)}
+                        alt="piece"
+                        width={200}
+                        height={200}
+                        className="w-full h-full"
+                      />
+                    </div>
+                  ) : (
+                    <div
+                      className={
+                        "w-full h-full " +
+                        (moves.includes(square) ? "bg-[#0077ff55]" : "")
                       }
-                    }}
-                  >
-                    {row + 1 == 8 && (
-                      <div
-                        className={`absolute bottom-0 right-1 text-sm ${
-                          (col + 1) % 2 == 0 ? "text-green-700" : "text-white"
-                        }`}
-                      >
-                        {square.split("")[0]}
-                      </div>
-                    )}
-                    {col == 0 && (
-                      <div
-                        className={`absolute top-0 left-1 text-sm ${
-                          (row + 1) % 2 == 0 ? "text-white" : "text-green-700"
-                        }`}
-                      >
-                        {square.split("")[1]}
-                      </div>
-                    )}
-                    <Image
-                      src={pieceImageData(piece.type, piece.color)}
-                      alt="piece"
-                      width={200}
-                      height={200}
-                      className="w-full h-full"
-                    />
-                  </div>
-                ) : (
-                  <div
-                    className={
-                      "w-full h-full " +
-                      (moves.includes(square) ? "bg-[#0077ff55]" : "")
-                    }
-                    onClick={() => {
-                      if (moves.length > 0) {
-                        chess.move({ from: currentPosition, to: square });
-                        setMoves([]);
-                        setBoardArray(chess.board());
-                      }
-                    }}
-                  >
-                    {col == 0 && (
-                      <div
-                        className={`absolute top-0 left-1 text-sm ${
-                          (row + 1) % 2 == 0 ? "text-white" : "text-green-700"
-                        }`}
-                      >
-                        {square.split("")[1]}
-                      </div>
-                    )}
-                    <Image
-                      src={pieceImageData("p", "b")}
-                      alt="piece"
-                      width={200}
-                      height={200}
-                      className="w-full h-full opacity-0"
-                    />
-                  </div>
-                )}
-              </div>
-            );
+                      onClick={() => {
+                        if (moves.length > 0) {
+                          chess.move({ from: currentPosition, to: square });
+                          setMoves([]);
+                          setBoardArray(chess.board());
+                        }
+                      }}
+                    >
+                      {colIndex === 0 && (
+                        <div
+                          className={`absolute top-0 left-1 text-sm ${
+                            (rowIndex + 1) % 2 === 0
+                              ? "text-white"
+                              : "text-green-700"
+                          }`}
+                        >
+                          {square[1]}
+                        </div>
+                      )}
+                      <Image
+                        src={pieceImageData("p", "b")}
+                        alt="piece"
+                        width={200}
+                        height={200}
+                        className="w-full h-full opacity-0"
+                      />
+                    </div>
+                  )}
+                </div>
+              );
+            });
           })}
         </div>
       </div>
